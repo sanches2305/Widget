47504b045d947743c5326b5acf38e3ce305947e4
sanches2305 <44681656+sanches2305@users.noreply.github.com>
2023-02-02 20:22:06 +0200

    Update cod.js

--- cod.js
+++ cod.js
@@ -1,28 +1,28 @@
-// var menuBtn = $('.messenger-btn'),
-//       menu    = $('.messenger-links');
-//   menuBtn.on('click', function() {
-//       if ( menu.hasClass('show') ) {
-//           menu.removeClass('show');
-//       } else {
-//           menu.addClass('show');
-//       }
-//   });
-//   $(document).mouseup(function (e){ 
-//     var div = $('.messenger'); 
-//     if (!div.is(e.target) 
-//         && div.has(e.target).length === 0) {
-//       $('.messenger-links').removeClass('show');
-//     }
-//   });
-
-const menubtn = document.getElementById("menubtn");
-const menu = document.getElementById("messenger-links");
-console.log(menubtn); 
-
-menubtn.onclick = function() {
-      if ( menu.classList.contains('show') ) {
-          menu.classList.remove('show');
+var menuBtn = $('.messenger-btn'),
+      menu    = $('.messenger-links');
+  menuBtn.on('click', function() {
+      if ( menu.hasClass('show') ) {
+          menu.removeClass('show');
       } else {
-          menu.classList.add('show');
+          menu.addClass('show');
       }
-  };
+  });
+  $(document).mouseup(function (e){ 
+    var div = $('.messenger'); 
+    if (!div.is(e.target) 
+        && div.has(e.target).length === 0) {
+      $('.messenger-links').removeClass('show');
+    }
+  });
+
+// const menubtn = document.getElementById("menubtn");
+// const menu = document.getElementById("messenger-links");
+// console.log(menubtn); 
+
+// menubtn.onclick = function() {
+//       if ( menu.classList.contains('show') ) {
+//           menu.classList.remove('show');
+//       } else {
+//           menu.classList.add('show');
+//       }
+//   };
