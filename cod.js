f986037fe3db3bcb5ff9ba663304631f4ce8ab5c
sanches2305 <44681656+sanches2305@users.noreply.github.com>
2023-02-02 19:50:20 +0200

    Update cod.js

--- cod.js
+++ cod.js
@@ -1,16 +1,29 @@
-var menuBtn = $('.messenger-btn'),
-      menu    = $('.messenger-links');
-  menuBtn.on('click', function() {
-      if ( menu.hasClass('show') ) {
-          menu.removeClass('show');
+// var menuBtn = $('.messenger-btn'),
+//       menu    = $('.messenger-links');
+//   menuBtn.on('click', function() {
+//       if ( menu.hasClass('show') ) {
+//           menu.removeClass('show');
+//       } else {
+//           menu.addClass('show');
+//       }
+//   });
+//   $(document).mouseup(function (e){ 
+//     var div = $('.messenger'); 
+//     if (!div.is(e.target) 
+//         && div.has(e.target).length === 0) {
+//       $('.messenger-links').removeClass('show');
+//     }
+//   });
+
+const menubtn = document.getElementById("menubtn");
+const menu = document.getElementById("messenger-links");
+
+ 
+
+menubtn.onclick = function() {
+      if ( menu.classList.contains('show') ) {
+          menu.classList.remove('show');
       } else {
-          menu.addClass('show');
+          menu.classList.add('show');
       }
-  });
-  $(document).mouseup(function (e){ 
-    var div = $('.messenger'); 
-    if (!div.is(e.target) 
-        && div.has(e.target).length === 0) {
-      $('.messenger-links').removeClass('show');
-    }
-  });
+  };
